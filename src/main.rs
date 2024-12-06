fdd7ee7f631762e8608d06f1e58c0964e17c9b44
e-strauss <92718421+e-strauss@users.noreply.github.com>
2024-12-06 14:10:34 +0100

    new code examples

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,93 @@ use matmul::dot;
 fn main() {
     println!("Hi");
     variable_test();
-    intro();
+    intro(true);
     let a = [1.0,3.0,4.0];
     let b = [3.0,4.0,5.0];
     println!("result: {}", dot(&a, &b));
 
-    println!("\n----------------------------------------\n")
+    println!("\n----------------------------------------\n");
+    println!("{}", first_word(&String::from("helllooooo Mike")));
+    println!("{}", first_word(&String::from("Mike")));
+    assert_eq!(fake_bin("45385593107843568"), "01011110001100111");
+    let result = repeat_str("Hi", 10);
+    println!("{result}");
+    println!("{}", greet("Greg", "Mike"));
+    println!("{}", greet("Mike", "Mike"));
+    println!("{}", high_and_low("1 2 3 4 5"));
+    println!("{}", high_and_low2("1 2 3 4 5"));
+    slices_test();
 }
 
-fn intro() {
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
+
+fn high_and_low(numbers: &str) -> String {
+    let mut min = i32::MAX;
+    let mut max = i32::MIN;
+    for i in numbers.split_whitespace() {
+        let number = i.parse::<i32>().unwrap();
+        if number < min {
+            min = number;
+        }
+        if number > max {
+            max = number;
+        }
+    }
+    format!("{} {}", min, max)
+}
+
+fn high_and_low2(numbers: &str) -> String {
+    use std::cmp;
+    let f = |(max, min), x| (cmp::max(max, x), cmp::min(min, x));
+
+    let answer = numbers
+        .split_whitespace();
+    let answer = answer
+        .map(|x| x.parse::<i32>().unwrap());
+    let answer = answer
+        .fold((i32::MIN, i32::MAX), f);
+    format!("{} {}", answer.0, answer.1)
+}
+
+fn fake_bin(s: &str) -> String {
+    s.chars().map(|c| if c < '5' {'0'} else {'1'}).collect()
+}
+
+fn repeat_str(src: &str, count: usize) -> String {
+    src.repeat(count)
+}
+
+fn greet(name: &str, owner: &str) -> String {
+    let greet = if name == owner {
+        "boss"
+    } else {
+        "guest"
+    };
+
+    format!("Hello {}", greet)
+}
+
+fn slices_test() {
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
+}
+
+fn intro(skip : bool) {
+    if skip {
+        return;
+    }
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=1);
